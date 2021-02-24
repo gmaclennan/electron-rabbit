e0100778a7193604039e1c18e68037f0a7a0e7dd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2021-02-24 18:26:59 +0000

    fix: Stricter run-time type-checking

--- src/background-ipc.js
+++ src/background-ipc.js
@@ -1,11 +1,12 @@
 const ipc = require('node-ipc')
+const pIsPromise = require('p-is-promise')
 
 /**
  * Create an IPC server, listening on the specified socketName. Returns a
  * promise that will resolve when the IPC server is listening
  *
  * @param {string} socketName Unique name for the socket. Any existing socket with this name will be deleted.
- * @param {{ [handlerName: string]: (...args: any[]) => Promise<any> }} handlers Object of String -> Handler. Handler should be an asyncronous function that returns a Promise.
+ * @param {{ [handlerName: string]: (...args: any[]) => Promise<unknown> }} handlers Object of String -> Handler. Handler should be an asyncronous function that returns a Promise.
  * @returns {Promise<void>}
  */
 function init (socketName, handlers) {
@@ -18,11 +19,61 @@ function init (socketName, handlers) {
     ipc.server.on('start', resolve)
     ipc.server.start()
 
+    /**
+     * @param {string} data
+     * @param {import('net').Socket} socket
+     */
     function handleMessage (data, socket) {
-      const msg = JSON.parse(data)
+      let msg
+      try {
+        msg = JSON.parse(data)
+      } catch (e) {
+        return onError(new Error('Could not parse IPC message: ' + data))
+      }
       const { id, name, args } = msg
 
-      var onError = (error) => {
+      if (typeof id !== 'string') {
+        console.error('IPC message is missing an id')
+        // Can't send a reply without an id, so need to fail silently
+        // TODO: Generic error handler channel?
+        return
+      }
+      if (typeof name !== 'string') {
+        return onError(new Error('IPC message is missing a name'))
+      }
+
+      const handler = handlers[name]
+
+      if (typeof handler !== 'function') {
+        console.warn('Unknown method: ' + msg.name)
+        ipc.server.emit(
+          socket,
+          'message',
+          JSON.stringify({ type: 'reply', id, result: null })
+        )
+      }
+
+      let promise
+      try {
+        promise = handlers[name](args)
+      } catch (err) {
+        return onError(err)
+      }
+
+      if (!pIsPromise(promise)) {
+        return onError(new Error(`Handler '${name}' did not return a promise`))
+      }
+
+      promise.then(result => {
+        ipc.server.emit(
+          socket,
+          'message',
+          JSON.stringify({ type: 'reply', id, result })
+        )
+      }).catch(onError)
+
+      /** @param {Error} error */
+      function onError (error) {
         console.warn('Error', name, args)
         console.error(error)
         // Up to you how to handle errors, if you want to forward
@@ -31,30 +82,6 @@ function init (socketName, handlers) {
           socket,
           'message',
           JSON.stringify({ type: 'error', id, result: error.message })
-        )
-      }
-
-      if (handlers[name]) {
-        let promise
-        try {
-          promise = handlers[name](args)
-        } catch (err) {
-          onError(err)
-        }
-
-        promise.then(result => {
-          ipc.server.emit(
-            socket,
-            'message',
-            JSON.stringify({ type: 'reply', id, result })
-          )
-        }).catch(onError)
-      } else {
-        console.warn('Unknown method: ' + name)
-        ipc.server.emit(
-          socket,
-          'message',
-          JSON.stringify({ type: 'reply', id, result: null })
         )
       }
     }
